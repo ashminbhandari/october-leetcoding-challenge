a238ff14ef0a2edb9b229685644d7327b9b1886f
ashminbhandari <abhandar@ramapo.edu>
2020-10-06 20:20:48 -0500

    #6 insert into bst

---
+++ insert-into-bst.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def insertIntoBST(self, root, val):
+        if (root == None):
+            return TreeNode(val)
+
+        def helper(root, val):
+            if (root.val > val):
+                if (root.left): helper(root.left, val)
+                else:
+                    root.left = TreeNode(val)
+                    return
+            else:
+                if(root.right): helper(root.right, val)
+                else:
+                    root.right = TreeNode(val)
+                    return
+
+        helper(root, val)
+
+        return root
+                    
