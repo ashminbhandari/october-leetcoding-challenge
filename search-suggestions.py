7775c77b67513a19093314175e8be602c7a84b5f
ashminbhandari <abhandar@ramapo.edu>
2020-10-17 18:40:18 -0400

    #1268 - search suggestions system

---
+++ search-suggestions.py
@@ -0,0 +1,54 @@
+#create a new TrieNode class
+class TrieNode:
+    def __init__(self):
+        self.children = OrderedDict()
+        self.words = []
+
+    #trie class with ability to search as well as insert
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        node = self.root
+
+        for letter in word:
+            if letter not in node.children:
+                node.children[letter] = TrieNode()
+            node = node.children[letter]
+            node.words.append(word)
+            node.words.sort()
+            if len(node.words) > 3:
+                node.words.pop()
+
+    def search(self, word):
+        node = self.root
+        payload = []
+        for letter in word:
+            if letter in node.children:
+                node = node.children[letter]
+                payload.append(node.words)
+            else:
+                for i in range(len(word) - len(payload)):
+                    payload.append([])
+                break
+        return payload
+
+
+class Solution(object):
+    def suggestedProducts(self, products, searchWord):
+        trie = Trie()
+        for product in products:
+            trie.insert(product)
+        return trie.search(searchWord)
+
+
+
+
+
+
+
+
+
+
+
