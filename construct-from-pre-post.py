b5326dbba30517b8743015e3b19b51d769b53ccc
ashminbhandari <abhandar@ramapo.edu>
2020-11-11 19:59:52 -0500

    #create a binary tree from pre and post order traversal arrays

--- construct-from-pre-post.py
+++ construct-from-pre-post.py
@@ -1,20 +1,11 @@
 class Solution(object):
     def constructFromPrePost(self, pre, post):
-
         if not pre: return None
-
         root = TreeNode(pre[0])
-
         if len(pre) == 1: return root
-
         idx = post.index(pre[1]) + 1 #idx of second element in pre in post
-
-        print idx
-
         root.left = self.constructFromPrePost(pre[1 : idx + 1], post[ : idx])
-
         root.right = self.constructFromPrePost(pre[idx + 1 : ], post[idx : -1])
-
         return root
 
 
