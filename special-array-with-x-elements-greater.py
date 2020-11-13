faa082840349bf7b7b5cd66c4c6ea7dbf63d7abb
ashminbhandari <abhandar@ramapo.edu>
2020-11-12 21:14:50 -0500

    #1608. Special Array With X Elements Greater Than or Equal X #comments

--- special-array-with-x-elements-greater.py
+++ special-array-with-x-elements-greater.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def specialArray(self, nums):
         nums.sort()
-        i = 0
-        j = 0
-        while i <= nums[len(nums) - 1]:
-            if len(nums) - j == i:
+        i = 0 #range 0 to max (numx)
+        j = 0 #array iterator idx
+        while i <= nums[len(nums) - 1]: #go through entire range
+            if len(nums) - j == i: #if there are exactly i elements remaining from current idx in the sorted array
                 return i
-            while(j < len(nums) and nums[j] == i):
+            while(j < len(nums) and nums[j] == i): #if in the array we reach a value representation of i, increase j
                 j += 1
             i += 1
-            if i > len(nums) - j:
+            if i > len(nums) - j: #if the i we have is more than the elements remain, break out
                 break
         return -1
