354cd0912567afe3c4c34d7555d7cc29d7ff41c5
ashminbhandari <abhandar@ramapo.edu>
2020-11-19 13:51:29 -0500

    #med - group people given the group size they belong to

---
+++ group-people-given-groupsize-they-belong-to.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupThePeople(self, groupSizes):
+        m = {}
+        for i in range(len(groupSizes)):
+            print m
+            if groupSizes[i] not in m:
+                m[groupSizes[i]] = [i]
+            else:
+                m[groupSizes[i]].append(i)
+
+        ans = []
+        for k in m.keys():
+            arr = m[k]
+            i = 0
+            while i < len(arr):
+                ans.append(arr[i:i+k])
+                i += k
+        return ans
